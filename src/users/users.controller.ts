8463daf54736a92957ecf1fedc10b01096795d0c
Elias <manchai.fung@gmail.com>
2025-05-25 19:13:57 -0400

    init: users endpoint and structure

---
+++ src/users/users.controller.ts
@@ -0,0 +1,29 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+
+@Controller('users')
+export class UsersController {
+  @Get()
+  findAll(): string {
+    return 'This action returns all users';
+  }
+
+  @Post()
+  create(@Body() createUserDto: any) {
+    return 'This action adds a new user';
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return `This action returns user #${id}`;
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateUserDto: any) {
+    return `This action updates user #${id}`;
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return `This action removes user #${id}`;
+  }
+}
