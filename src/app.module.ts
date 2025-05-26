412058c18eaa933399d5e5418c3cd49e27c43b79
Jesus Ventura <jesusventura2005@gmail.com>
2025-05-26 02:07:46 -0400

    feat: added cars to db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,27 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { MongooseModule } from '@nestjs/mongoose';
+import { join } from 'path';
 import { UsersModule } from './users/users.module';
 import { CarsModule } from './cars/cars.module';
 
 @Module({
-  imports: [UsersModule, CarsModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),
+    MongooseModule.forRootAsync({
+      useFactory: () => ({
+        uri: process.env.MONGODB_URI,
+        dbName: process.env.DB_NAME,
+      }),
+    }),
+    UsersModule,
+    CarsModule,
+  ],
 })
 export class AppModule {}
--- src/cars/cars.module.ts
+++ src/cars/cars.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CarsService } from './cars.service';
 import { CarsController } from './cars.controller';
+import { carSchema, Car } from './entities/car.entity';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
+  imports: [MongooseModule.forFeature([{ name: Car.name, schema: carSchema }])],
   controllers: [CarsController],
   providers: [CarsService],
 })
