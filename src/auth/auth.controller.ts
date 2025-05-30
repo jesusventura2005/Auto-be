cc6cb9759a1bac768cb7e1045cdbf4ddbc7a63cc
Elias <manchai.fung@gmail.com>
2025-05-30 17:02:04 -0400

    minor fix

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,5 @@
-import { Controller, Post, Body, HttpCode, HttpStatus, UseGuards, Request, BadRequestException } from '@nestjs/common';
+import { Controller, Post, Body, HttpCode, HttpStatus, BadRequestException } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { AuthGuard } from '@nestjs/passport';
 import { CreateUserDto } from 'src/users/dto/create-user.dto';
 import { LogInDto } from './dto/login.dto';
 
@@ -8,14 +7,12 @@ import { LogInDto } from './dto/login.dto';
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @UseGuards(AuthGuard('local'))
   @HttpCode(HttpStatus.OK)
   @Post('login')
   signIn(@Body() loginUserDto: LogInDto) {
     return this.authService.logIn(loginUserDto);
   }
 
-  @UseGuards(AuthGuard('jwt'))
   @HttpCode(HttpStatus.OK)
   @Post('register')
   async register(@Body() registerDto: CreateUserDto) {
