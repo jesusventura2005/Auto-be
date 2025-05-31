a6f8f89c055d51201bc5047223f0cbddee48963f
Jesus Ventura <jesusventura2005@gmail.com>
2025-05-31 18:26:16 -0400

    feat: enable cors to test the api in the FE

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:4000/',
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  });
 
   const config = new DocumentBuilder()
     .setTitle('Autos')
