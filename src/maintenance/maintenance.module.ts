3f8b7020ed1c28fc96230a9757160d26678dd1b4
Elias Fung <manchai.fung@gmail.com>
2025-07-03 08:14:32 -0400

    testing again

--- src/maintenance/maintenance.module.ts
+++ src/maintenance/maintenance.module.ts
@@ -3,9 +3,15 @@ import { MaintenanceService } from './maintenance.service';
 import { MaintenanceController } from './maintenance.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { MaintenanceSchema } from './entities/maintenance.entity';
+import { carSchema } from 'src/cars/entities/car.entity';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: 'Maintenance', schema: MaintenanceSchema }]), MaintenanceModule],
+  imports: [
+    MongooseModule.forFeature([
+      { name: 'Maintenance', schema: MaintenanceSchema },
+      { name: 'Car', schema: carSchema },
+    ]),
+  ],
   controllers: [MaintenanceController],
   providers: [MaintenanceService],
 })
